e13031d4d71ee5c925a48b6ee239ec3ba0250371
alice0520 <alice900531@gmail.com>
2022-12-04 17:57:32 +0800

    Add Global Map GeoJson and Show Map

--- main.js
+++ main.js
@@ -4,3 +4,33 @@ const height = 710;
 const svg = d3.select("#chart-area").append("svg")
             .attr("width", width)
             .attr("height", height);
+
+//// Map
+var teams = {};
+d3.json("global_map.json").then(geoData => {
+    var width = 600;
+    var height = 400;
+
+    moveX = 0;
+    moveY = 80;
+
+    const gMap = svg.append("g")
+                    .attr("transform", `translate(${moveX}, ${moveY})`);
+
+    var projection = d3.geoMercator()
+                        .fitExtent([[0,0], [width, height]], geoData);
+
+    var geoGenerator = d3.geoPath()
+                        .projection(projection);
+
+    gMap.append("g")
+        .selectAll("path")
+        .data(geoData.features)
+        .enter()
+        .append("path")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("stroke", "black")
+        .attr("fill", "white")
+        .attr("d", geoGenerator);
+});
