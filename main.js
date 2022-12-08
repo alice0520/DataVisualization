3b423361d9d6ed4360884b74449f575ee27bce8a
QQ1010 <asdfg335522@gmail.com>
2022-12-08 17:19:59 +0800

    Add: change view

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ const height = 110;
 const svg = d3.select("#chart-area").append("svg")
             .attr("width", width)
             .attr("height", height);
+
+let select = document.querySelector("div .select_view");
+select.addEventListener("change", selectFun);
+
+function selectFun() {
+    const switchValue = select.options[select.selectedIndex].value;
+    console.log(switchValue)
+    // if(switchValue == "global") {
+    //     document.getElementById("chart-area").style = "display:none";
+    // }
+    if(switchValue == "local") {
+        document.getElementById("map_checkbox_piechart").style = "display:none";
+        document.getElementById("bar_chart_global_view").style = "display:none";
+
+    }
+}
