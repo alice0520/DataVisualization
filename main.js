9d8b1be6bab7fb026d76db7bb0dbbcfe14407aaa
alice0520 <alice900531@gmail.com>
2022-12-10 11:30:53 +0800

    fix: data Guinea's continent "America" to "Africa"
    data

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 const width = 1300;
 const height = 110;
 
-// const svg = d3.select("#chart-area").append("svg")
-//             .attr("width", width)
-//             .attr("height", height);
-
 let select = document.querySelector("div #view");
 select.addEventListener("change", selectFun);
 
@@ -24,6 +20,5 @@ function selectFun() {
         document.getElementById("bar_chart_global_view").style = "display:none";
         document.getElementById("local-view").style = "display:show";
         document.getElementById("continent").style = "display:show";
-        loadLocalView();
     }
 }
