fef833a269afbe1aeafea4a0b3e975d7c639eed2
alice0520 <alice900531@gmail.com>
2022-12-11 17:43:21 +0800

    add line chart transition

--- local_view_up.js
+++ local_view_up.js
@@ -5,8 +5,6 @@ const local_svg = d3.select("#local-view").append("svg")
             .attr("width", local_width)
             .attr("height", local_height);
 
-var continent = "Global";
-
 //// Map
 data_map = d3.json("world-map.json");
 drawMap();
@@ -20,6 +18,8 @@ loc_data = d3.csv("world_country_loc.csv");
 var year = 2015;
 var is_click = false;
 var continent = "Global";
+var feature = "happiness_score"
+var years = [2015, 2016, 2017, 2018, 2019, 2020];
 
 drawLineChart();
 
@@ -55,6 +55,7 @@ function getDataByYear(year){
 function changeContinent(){
     continent = document.getElementById("continent").value;
     changeMapCircleColor();
+    updateLineChart(feature, continent);
 }
 
 function changeYear(){
@@ -108,8 +109,6 @@ function drawMap(){
 
         d3.selectAll("path")
             .on("click", function(){
-                //choose continent
-                //call Qin's function
                 if(continent == this.className.baseVal || (continent == "America" && (this.className.baseVal == "North America" || this.className.baseVal == "South America"))){
                     is_click = true;
                 }
@@ -125,9 +124,11 @@ function drawMap(){
                         continent = "America";
                     }
                 }
-                // console.log(continent);
                 document.getElementById("continent").value = continent;
                 changeMapCircleColor();
+                updateLineChart(feature, continent);
+                //choose continent
+                //call Qin's function
             });
             
         data_by_year = getDataByYear(year);
@@ -155,8 +156,6 @@ function drawMap(){
                                 return d.happiness_score * 2;
                             })
                             .on("click", function(){
-                                //choose continent
-                                //call Qin's function
                                 if(continent == this.className.baseVal){
                                     is_click = true;
                                 }
@@ -169,9 +168,11 @@ function drawMap(){
                                 else{
                                     continent = this.className.baseVal;
                                 }
-                                // console.log(continent);
                                 document.getElementById("continent").value = continent;
                                 changeMapCircleColor();
+                                updateLineChart(feature, continent);
+                                //choose continent
+                                //call Qin's function
                             });
 
                 var tip = d3.tip()
@@ -235,10 +236,11 @@ function changeMapCircleColor(){
     })
 }
 
+var line_width = 400;
+var line_height = 300;
+
 function drawLineChart(){
-    var allFeature = ["happiness_score", "gdp_per_capita", "family", "health", "freedom", "generosity", "government_trust", "dystopia_residual", "social_support", "cpi_score"]
     data.then(line_data => {
-        years = [2015, 2016, 2017, 2018, 2019, 2020];
         avg_list = [];
         years.forEach(year => {
             year_data = line_data.filter(function(d){return d.Year == year});
@@ -252,9 +254,6 @@ function drawLineChart(){
                 'value': avg
             });
         });
-
-        var line_width = 400;
-        var line_height = 300;
     
         moveX = 800;
         moveY = 100;
@@ -275,21 +274,21 @@ function drawLineChart(){
            .attr("transform", "translate(0," + line_height + ")")
            .call(d3.axisBottom(xScale).ticks(6).tickFormat(d=>String(d)));
          
-        gLine.append("g")
-            .call(d3.axisLeft(yScale));               
+        yAxis = gLine.append("g")
+                    .call(d3.axisLeft(yScale));            
          
         const lineGenerator = d3.line()
                                 .x(d => xScale(d.key))
                                 .y(d => yScale(d.value))
                                 .curve(d3.curveLinear);
 
-        gLine.append("path")
+        lines = gLine.append("path")
             .attr("fill", "none")
             .attr("stroke", "black")
             .attr("stroke-width", 3)
             .attr("d", lineGenerator(avg_list));
 
-        gLine.append("g")
+        points = gLine.append("g")
             .selectAll("dot")
             .data(avg_list)
             .enter()
@@ -299,4 +298,139 @@ function drawLineChart(){
               .attr("r", 5)
               .attr("fill", "#69b3a2")
     });
+}
+
+d3.select("#selectFeature").on("change", function(d) {
+    feature = d3.select(this).property("value");
+    updateLineChart(feature, continent);
+})
+
+function updateLineChart(feature, continent){
+    data.then(line_data => {
+        if(continent != "Global"){
+            line_data = line_data.filter(function(d){return d.continent == continent});
+        }
+        avg_list = [];
+        years.forEach(year => {
+            year_data = line_data.filter(function(d){return d.Year == year});
+            sum = 0;
+            year_data.forEach(d => {
+                if(feature == "happiness_score"){
+                    sum += d.happiness_score;
+                }
+                else if(feature == "gdp_per_capita"){
+                    sum += d.gdp_per_capita;
+                }
+                else if(feature == "family"){
+                    sum += d.family;
+                }
+                else if(feature == "health"){
+                 
+                    sum += d.health;
+                }
+                else if(feature == "freedom"){
+                    sum += d.freedom;
+                }
+                else if(feature == "generosity"){
+                    sum += d.generosity;
+                }
+                else if(feature == "government_trust"){
+                    sum += d.government_trust;
+                }
+                else if(feature == "dystopia_residual"){
+                    sum += d.dystopia_residual;
+                }
+                else if(feature == "social_support"){
+                    sum += d.social_support;
+                }
+                else if(feature == "cpi_score"){
+                    sum += d.cpi_score;
+                }
+            });
+            avg = sum / year_data.length;
+            avg_list.push({
+                'key': year,
+                'value': avg
+            });
+        });
+
+        var xScale = d3.scaleLinear()
+                        .domain(d3.extent(line_data, d=>d.Year))
+                        .range([0, line_width]);
+
+        var newy = d3.scaleLinear()
+                        .domain(d3.extent(avg_list, d=>d.value))
+                        .range([line_height, 0]);
+        
+        if(feature == "happiness_score"){
+            newy = d3.scaleLinear()
+                        .domain(d3.extent(line_data, d=>d.happiness_score))
+                        .range([line_height, 0]);
+        }
+        else if(feature == "gdp_per_capita"){
+            newy = d3.scaleLinear()
+                        .domain(d3.extent(line_data, d=>d.gdp_per_capita))
+                        .range([line_height, 0]);
+        }
+        else if(feature == "family"){
+            newy = d3.scaleLinear()
+                        .domain(d3.extent(line_data, d=>d.family))
+                        .range([line_height, 0]);
+        }
+        else if(feature == "health"){
+            newy = d3.scaleLinear()
+                        .domain(d3.extent(line_data, d=>d.health))
+                        .range([line_height, 0]);
+        }
+        else if(feature == "freedom"){
+            newy = d3.scaleLinear()
+                        .domain(d3.extent(line_data, d=>d.freedom))
+                        .range([line_height, 0]);
+        }
+        else if(feature == "generosity"){
+            newy = d3.scaleLinear()
+                        .domain(d3.extent(line_data, d=>d.generosity))
+                        .range([line_height, 0]);
+        }
+        else if(feature == "government_trust"){
+            newy = d3.scaleLinear()
+                        .domain(d3.extent(line_data, d=>d.government_trust))
+                        .range([line_height, 0]);
+        }
+        else if(feature == "dystopia_residual"){
+            newy = d3.scaleLinear()
+                        .domain(d3.extent(line_data, d=>d.dystopia_residual))
+                        .range([line_height, 0]);
+        }
+        else if(feature == "social_support"){
+            newy = d3.scaleLinear()
+                        .domain(d3.extent(line_data, d=>d.social_support))
+                        .range([line_height, 0]);
+        }
+        else if(feature == "cpi_score"){
+            newy = d3.scaleLinear()
+                        .domain(d3.extent(line_data, d=>d.cpi_score))
+                        .range([line_height, 0]);
+        }
+
+        var newyAxisCall = d3.axisLeft(newy);
+        
+        var t= d3.transition()
+                .duration(1000);
+        
+        yAxis.transition(t).call(newyAxisCall);
+
+        const lineGenerator = d3.line()
+                                .x(d => xScale(d.key))
+                                .y(d => newy(d.value))
+                                .curve(d3.curveLinear);
+
+        lines.transition(t)
+            .attr("d", lineGenerator(avg_list));
+
+        points.data(avg_list)
+            .transition(t)
+            .attr("cx", function(d) { return xScale(d.key) } )
+            .attr("cy", function(d) { return newy(d.value) } );
+    })
 }
